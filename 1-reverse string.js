d6241e5f54c708c4a1bc4726d5c9ae88b907b0e4
Ankita <ankietathorve@gmail.com>
2026-04-21 22:38:39 +0530

    Describe the problem

--- 1-reverse string.js
+++ 1-reverse string.js
@@ -1,5 +1,5 @@
-// --- write a program to reverse a string in JS.
-
+// --- Write a program to reverse a string in JS.
+//This code splits the string into an array of characters using the split() method, reverses the array, and joins it back into a string using the join() method.
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
