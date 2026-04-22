cd706a01ee3dcb94d54ee6094c11eb56a12cf5da
Ankita <ankietathorve@gmail.com>
2026-04-22 21:33:30 +0530

    Func to add property to an object

---
+++ 7-addPropToObj.js
@@ -0,0 +1,6 @@
+// ---Write a Program to add a property to an object?
+// The code creates an object obj with a name property set to 'Riya'. Then, it adds a new property age with the value 21. Finally, console.log(obj) prints the updated object, which now contains both name and age properties: { name: 'Riya', age: 21 }.
+
+const obj = { name: "Riya" };
+obj.age = 21;
+console.log(obj);
