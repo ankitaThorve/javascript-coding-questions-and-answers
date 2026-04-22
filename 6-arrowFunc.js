258b856ab855ec974c7102d3fe5f0aefa1a5f3e7
Ankita <ankietathorve@gmail.com>
2026-04-22 21:18:33 +0530

    Arrow func

---
+++ 6-arrowFunc.js
@@ -0,0 +1,5 @@
+// ---Write a code to create an arrow function?
+// The code defines an arrow function add that takes two arguments a and b and returns their sum (a + b). When console.log(add(6, 2)) is executed, it calls the add function with 6 and 2, and prints the result which is 8, to the console.
+
+const add = (a, b) => a + b;
+console.log(add(6, 2));
