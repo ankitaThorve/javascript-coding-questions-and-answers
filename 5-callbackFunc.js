5daf2802a99837ca5c2f9659e915dba426c2727b
Ankita <ankietathorve@gmail.com>
2026-04-22 21:14:56 +0530

    Question description

--- 5-callbackFunc.js
+++ 5-callbackFunc.js
@@ -1,3 +1,7 @@
+//---Write a Program to use a callback function?
+// This code defines a greet function that takes two arguments- a name and a callback function. It calls the callback with a greeting message using the name. When greet('Geek', message => console.log(message)) is executed, it prints "Hello, Geek!" as output.
+
+
 function greet(name, callback) {
   callback(`Hello, ${name}!`);
 }
