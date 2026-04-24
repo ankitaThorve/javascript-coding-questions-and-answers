cbc71d7f4f4e54a2c87da4f65980bedc15b6558f
Ankita <ankietathorve@gmail.com>
2026-04-24 23:49:24 +0530

    use reduce() on array

---
+++ 9-arrReduce.js
@@ -0,0 +1,2 @@
+// What will be the output of the given code?
+// The code uses the reduce method on the array [1, 2, 3] to sum its elements. It takes two parameters, a (accumulator) and b (current value), adding them together. The final result 6 is printed to the console, representing the total of the array's numbers.
