5f53839e198779c0e1a1087e57043800738f2c12
Ankita <ankietathorve@gmail.com>
2026-04-24 23:23:23 +0530

    console object

--- 8-deletePropFromObj.js
+++ 8-deletePropFromObj.js
@@ -1,5 +1,6 @@
 // --- Write a Program to delete a property from an object?
 // In this code, an object obj with properties name and age is created. The delete keyword removes the age property from the object. After deletion, console.log(obj) outputs the object, which now only contains the name property: { name: 'Riya' }.
 
-const obj = { name: "Ankita", age: 21 };
+const obj = { name: "Riya", age: 21 };
 delete obj.age;
+console.log(obj);
