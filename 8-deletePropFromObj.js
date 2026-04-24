946c2ae2e9e9e615a1d8a85ffdc0f993785ab1b7
Ankita <ankietathorve@gmail.com>
2026-04-24 23:22:34 +0530

    use dlete keyword

--- 8-deletePropFromObj.js
+++ 8-deletePropFromObj.js
@@ -2,3 +2,4 @@
 // In this code, an object obj with properties name and age is created. The delete keyword removes the age property from the object. After deletion, console.log(obj) outputs the object, which now only contains the name property: { name: 'Riya' }.
 
 const obj = { name: "Ankita", age: 21 };
+delete obj.age;
