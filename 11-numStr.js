6e145df99da189900703c2d95299ef1d39ae5911
Ankita <ankietathorve@gmail.com>
2026-04-28 21:39:42 +0530

    console

--- 11-numStr.js
+++ 11-numStr.js
@@ -14,4 +14,4 @@ console.log(1 === "1");
 // ---What will be the output of the given code?
 // 'null' represents the absence of any value and 'undefined' represents a variable that has been declared but not assigned any value. The answer is true because JavaScript treats them equal because of '==' loose equality operator.
 
-console.log(null == undefined)
+console.log(null == undefined);
