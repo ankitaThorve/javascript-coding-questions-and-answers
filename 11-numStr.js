0c6f373902c5620f0630ba9e8a72e611dd9380c4
Ankita <ankietathorve@gmail.com>
2026-04-28 21:23:45 +0530

    console sum of num & string

---
+++ 11-numStr.js
@@ -0,0 +1,5 @@
+// ---What will be the output of the given code?
+// The code console.log(1 + '2'); adds the number 1 and the string '2'. In JavaScript, when a number and a string are combined with +, the number is converted to a string. Finally, it results in the string '12', which is then prints to the console.
+console.log(1 + "2");
+
+
