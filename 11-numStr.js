9aa80e4d866ee8d0c676248e486f7cf0e1e6fb4f
Ankita <ankietathorve@gmail.com>
2026-04-28 21:29:48 +0530

    console substraction  of string string & num

--- 11-numStr.js
+++ 11-numStr.js
@@ -2,4 +2,7 @@
 // The code console.log(1 + '2'); adds the number 1 and the string '2'. In JavaScript, when a number and a string are combined with +, the number is converted to a string. Finally, it results in the string '12', which is then prints to the console.
 console.log(1 + "2");
 
+// ---What will be the output of the given code?
+// '6' is a string, so when you use the '-' operator with string and number, JavaScript convert the string to a number automatically which is called type coercion. '6' gets converted to the number 6, then 6 - 1 = 5. So, 5 is the answer.
 
+console.log("6" - 1);
