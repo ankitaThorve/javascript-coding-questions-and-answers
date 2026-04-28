4b9530c4dbb89847d86d65b1e05c8cb9f91b36ee
Ankita <ankietathorve@gmail.com>
2026-04-28 21:32:25 +0530

    check strict equality operator

--- 11-numStr.js
+++ 11-numStr.js
@@ -6,3 +6,7 @@ console.log(1 + "2");
 // '6' is a string, so when you use the '-' operator with string and number, JavaScript convert the string to a number automatically which is called type coercion. '6' gets converted to the number 6, then 6 - 1 = 5. So, 5 is the answer.
 
 console.log("6" - 1);
+
+// ---What will be the output of the given code?
+// '1' is a integer and '1' is a string. Strict equality operator(===) check both the type and the value. They look similar but they have different data type. So =, the answer in false.
+console.log(1 === "1");
