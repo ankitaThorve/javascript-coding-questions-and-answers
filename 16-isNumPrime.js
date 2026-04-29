70db3da41faf1aabd34d605c9d70375dbf44d869
Ankita <ankietathorve@gmail.com>
2026-04-29 21:37:35 +0530

    check num is prime or not

---
+++ 16-isNumPrime.js
@@ -0,0 +1,2 @@
+// --- Write a program to check if a Number is prime or not?
+// The isPrime() function checks if a number num is prime. It returns false for numbers less than or equal to 1. It loops starts from 2 to num - 1, checking if num is divisible by any number in that range. If it is, it returns false otherwise, it returns true.
