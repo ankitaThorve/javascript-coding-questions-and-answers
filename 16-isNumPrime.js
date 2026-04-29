ca182559b9daa4e9ff1db4841afaa48058daa72e
Ankita <ankietathorve@gmail.com>
2026-04-29 21:46:04 +0530

    function with for loop

--- 16-isNumPrime.js
+++ 16-isNumPrime.js
@@ -1,2 +1,17 @@
 // --- Write a program to check if a Number is prime or not?
 // The isPrime() function checks if a number num is prime. It returns false for numbers less than or equal to 1. It loops starts from 2 to num - 1, checking if num is divisible by any number in that range. If it is, it returns false otherwise, it returns true.
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (i % num === 0) {
+      return false;
+    }
+    return true;
+  }
+}
+
+console.log(isPrime(7));
+console.log(isPrime(4));
+console.log(isPrime(2));
